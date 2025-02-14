afd705f0b87a3e7a3c9f4e826e2f0bbac41bf568
leongpet <peter.leong@mail.utoronto.ca>
2025-02-14 00:01:34 -0500

    2025-02-14

--- Projects/Project_1/main.c
+++ Projects/Project_1/main.c
@@ -1,65 +1,73 @@
 #include "autocomplete.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(void)
 {
     struct term *terms;
     int nterms;
-    read_in_terms(&terms, &nterms, const char* filename){  
 
+    // Comparator function for qsort
+    int compare_terms(const void *a, const void *b) {
+        return strcmp(((struct term*)a)->term, ((struct term*)b)->term);
+    }
+    
+    void read_in_terms(struct term **p_p_terms, int *p_nterms, const char *filename) {
         int i = 0;
-        
+    
         // Open the file "filename.txt"
-        FILE* fp = fopen(filename, "r")
-        if (file == NULL){
-            printf("Error openining file\n");
+        FILE *fp = fopen(filename, "r");
+        if (fp == NULL) {
+            printf("Error opening file\n");
+            return;  // Return if file opening fails
         }
-
-        // Count the number of terms and store it in *pnterms
-        char line[200]; // Given to assume line <= 200
-        *pnterms = 0;
-        while(fgets(line, sizeof(line), fp)) {
-            (*pnterms)++;
+    
+        // Count the number of terms and store it in *p_nterms
+        char line[200]; // Assumes line is <= 200 characters
+        *p_nterms = 0;
+        while (fgets(line, sizeof(line), fp)) {
+            (*p_nterms)++;
         }
-        
-        // Allocate memory
-        *terms = (term*)malloc(*pnterms * sizeof(term))
-        if (*terms == NULL){
+    
+        // Allocate memory for terms
+        *p_p_terms = (struct term*)malloc(*p_nterms * sizeof(struct term));
+        if (*p_p_terms == NULL) {
             printf("Memory allocation failed\n");
             fclose(fp);
+            return;  // Return if memory allocation fails
         }
-        // Rewind file reading back to the start
-        // so that terms can be stored in *terms
+    
+        // Rewind file to read terms into the allocated array
         rewind(fp);
-
+    
         // Read data into allocated array
-        for (i; i < *pnterms; i++){
-            if(fgets(line, sizeof(line), fp)){
-                // sscanf parses through the entire line to grab both datum
-                sscanf(line, "%lf\t%199s", &(*terms)[i].weight. (*terms).);
+        for (i = 0; i < *p_nterms; i++) {
+            if (fgets(line, sizeof(line), fp)) {
+                // sscanf parses the line to extract the weight and term
+                sscanf(line, "%lf\t%199s", &(*p_p_terms)[i].weight, (*p_p_terms)[i].term);
             }
         }
-
+    
         fclose(fp);
-        
+    
         // Sort the terms lexicographically using strcmp
-        qsort(*terms, *pnterms, sizeof(term), 
-        (int (*)(const void*, const void*)) 
-        (int (*)(const void*, const void*)) 
-            (strcmp(((term*)a)->term, ((term*)b)->term)));
+        qsort(*p_p_terms, *p_nterms, sizeof(struct term),
+            (int (*)(const void *, const void *))compare_terms);
     }
-
-    int lowest_match(term* terms, int nterms, char* substr) {
+    
+    int lowest_match(struct term* p_p_terms, int nterms, const char* substr) {
+        if (nterms == 0) return -1;  // Handle case where there are no terms
+        
         int left = 0;
         int right = nterms - 1;
         int res = -1;
         int len_substr = strlen(substr);
-
+    
         while (left <= right) {
             int mid = (left + right) / 2;
-            int cmp = strncmp(terms[mid].term, substr, len_substr);
-
+            int cmp = strncmp(p_p_terms[mid].term, substr, len_substr);
+    
             if (cmp < 0) {
                 // The term at mid is lexicographically less than the prefix.
                 left = mid + 1;
@@ -69,38 +77,37 @@ int main(void)
                 right = mid - 1;
             }
         }
-
+    
         // Verify that the candidate actually starts with the prefix.
-        if (res != -1 && strncmp(terms[res].term, substr, len_substr) == 0) {
+        if (res != -1 && strncmp(p_p_terms[res].term, substr, len_substr) == 0) {
             return res;
         }
         return -1;
     }
 
-    highest_match(struct term* terms, int nterms, char* substr){
+    int highest_match(struct term* terms, int nterms, const char* substr) {
         int left = 0;
         int right = nterms - 1;
         int res = -1;
         int len_substr = strlen(substr);
-
+    
         while (left <= right) {
             int mid = (left + right) / 2;
             int cmp = strncmp(terms[mid].term, substr, len_substr);
-
-            if (cmp <= 0) {
-                // Mid is lexicographically less or equal to substr
-                if (cmp == 0){
-                    // Update res and continue looking right
+    
+            if (cmp < 0) {
+                // Mid is lexicographically less than substr
+                left = mid + 1;
+            } else {
+                // Mid is lexicographically greater or equal to substr
+                if (cmp == 0) {
+                    // Update res and continue searching to the right
                     res = mid;
                 }
-                left = mid + 1;
-            }
-            else {
-                // Mid is lexicographically more than substr
-                right = mid - 1
+                right = mid - 1;
             }
         }
-
+    
         // Verify that the candidate actually starts with the prefix.
         if (res != -1 && strncmp(terms[res].term, substr, len_substr) == 0) {
             return res;
@@ -108,9 +115,41 @@ int main(void)
         return -1;
     }
     
+    // Don't change (intitializing variables)
     struct term *answer;
     int n_answer;
-    autocomplete(&answer, &n_answer, terms, nterms, "Tor");
+    
+    // Comparator function to sort by weight (non-decreasing order)
+    int compare_by_weight(const void *a, const void *b) {
+        double weight_a = ((struct term*)a)->weight;
+        double weight_b = ((struct term*)b)->weight;
+        return (weight_a > weight_b) - (weight_a < weight_b);
+    }
+
+    void autocomplete(term** p_p_answer, int* p_n_answer, term* p_terms, int nterms, char* p_substr){
+        int start = lowest_match(p_terms, nterms, p_substr); // Use 'start' to hold the index.
+        if (start == -1) {
+            *p_p_answer = NULL;
+            *p_n_answer = 0;
+            return;
+        }
+    
+        int i = start;
+        int count = 0;
+        while (i < nterms && strncmp(p_terms[i].term, p_substr, strlen(p_substr)) == 0) {
+            count++;
+            i++;
+        }
+    
+        *p_p_answer = (term*)malloc(count * sizeof(term));
+        *p_n_answer = count;
+    
+        for (i = 0; i < count; i++) {
+            (*p_p_answer)[i] = p_terms[start + i];
+        }
+    
+        qsort(*p_p_answer, *p_n_answer, sizeof(term), compare_by_weight);
+    }    
     
     //free allocated blocks here -- not required for the project, but good practice
 
